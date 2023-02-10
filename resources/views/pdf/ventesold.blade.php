a31954b141cf8e959881f493722e93f005faed23
Alioune badara dieng <badaralune9@gmail.com>
2023-02-10 02:35:31 +0100

    Update

--- resources/views/pdf/ventesold.blade.php
+++ resources/views/pdf/ventesold.blade.php
@@ -52,8 +52,8 @@
                 <div>
                     <p class="badge" style="text-align:left;line-height:15px">Client</p>
                     <p style="text-align:left;line-height:5px">{{ $client ?  "Nom: " .\App\Models\Outil::premereLettreMajuscule($client["nom_complet"]) : "CLIENT DE PASSAGE"}}</p>
-                    <p style="text-align:left ;line-height:5px ">{{ $client ? "Téléphone: " . $client["telephone"] : " "}}</p>
-                    <p style="text-align:left;line-height:5px;text-transform: capitalize "> {{ $client ? "Adresse:" . $client["adresse"] : ""}}</p>
+                    <p style="text-align:left ;line-height:5px ">{{ $client["telephone"] ? "Téléphone: " . $client["telephone"] : " "}}</p>
+                    <p style="text-align:left;line-height:5px;text-transform: capitalize "> {{ $client["adresse"] ? "Adresse:" . $client["adresse"] : ""}}</p>
                 </div>
             </td>
             <td style="border:none;"></td>
